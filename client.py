e254ceed45d125ed33cf0201e7dadde84e35bd2b
paradox-shivansh <theaverageguy19@gmail.com>
2026-08-09 04:07:52 +0530

    client is finally wroking

--- client.py
+++ client.py
@@ -1,31 +1,55 @@
-from langchain_mcp_adapters.client import MultiServerMCPClient
+import os
 
 from dotenv import load_dotenv
-import os
-import asyncio
+from langchain_groq import ChatGroq
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain.agents import create_agent
+
 
 load_dotenv()
 
 
 async def main():
 
-    client = MultiServerMCPClient(
-        {
-            "math": {
-                "command": "python mathserver.py",
-                "args": [
-                    r"D:\LEARNING_HOW_TO_CODE\MCP_AWS\mathserver.py"
-                ],
-                "transport": "stdio",
-            },
+    client = MultiServerMCPClient({
 
-            "weather": {
-                "url": "http://localhost:8000/mcp",
-                "transport": "streamable_http",
-            },
-        }
+        "math": {
+            "command": "python",
+            "args": [
+                r"D:\LEARNING_HOW_TO_CODE\MCP_AWS\mathserver.py"
+            ],
+            "transport": "stdio",
+        },
+
+        "weather": {
+            "url": "http://localhost:8000/mcp",
+            "transport": "streamable_http",
+        },
+    })
+
+    tools = await client.get_tools()
+
+    model = ChatGroq(
+        model="llama-3.3-70b-versatile"
     )
+
+    agent = create_agent(
+        model,
+        tools
+    )
+
+    response = await agent.ainvoke({
+        "messages": [
+            {
+                "role": "user",
+                "content": "What is 25 multiplied by 4?"
+            }
+        ]
+    })
+
+    print(response)
 
 
 if __name__ == "__main__":
+    import asyncio
     asyncio.run(main())
