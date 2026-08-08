88e3683d4f2b9efc4852f224e818e0af9d4c76c2
paradox-shivansh <theaverageguy19@gmail.com>
2026-08-09 03:57:19 +0530

    building the multi server client

--- client.py
+++ client.py
@@ -1,15 +1,31 @@
-# from langchain_mcp_adapters.client import MCPClient
-from mcp.server.fastmcp import FastMCP
-from langchain_groq import ChatGroq
 from langchain_mcp_adapters.client import MultiServerMCPClient
 
 from dotenv import load_dotenv
 import os
-load_dotenv()
-
 import asyncio
 
+load_dotenv()
+
+
 async def main():
-    client =  MultiServerMCPClient(
-        # pass
+
+    client = MultiServerMCPClient(
+        {
+            "math": {
+                "command": "python mathserver.py",
+                "args": [
+                    r"D:\LEARNING_HOW_TO_CODE\MCP_AWS\mathserver.py"
+                ],
+                "transport": "stdio",
+            },
+
+            "weather": {
+                "url": "http://localhost:8000/mcp",
+                "transport": "streamable_http",
+            },
+        }
     )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
